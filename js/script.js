f1ec96f754a15410f25398333d56662c8ee77788
Janette Campbell <campbjp@hotmail.com>
2022-08-09 14:07:37 -0400

    chore: removed extra dashes

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 "use strict";
 // Selecting elements
-const score0El = document.querySelector("#score--0");
-const score1El = document.getElementById("score--1");
+const score0El = document.querySelector("#score-0");
+const score1El = document.getElementById("score-1");
 const diceEl = document.querySelector(".dice");
-const btnNew = document.querySelector(".btn--new");
-const btnRoll = document.querySelector(".btn--roll");
-const btnHold = document.querySelector(".btn--hold");
-const current0El = document.getElementById("current--0");
-const current1El = document.getElementById("current--1");
-const player0El = document.querySelector(".player--0");
-const player1El = document.querySelector(".player--1");
+const btnNew = document.querySelector(".btn-new");
+const btnRoll = document.querySelector(".btn-roll");
+const btnHold = document.querySelector(".btn-hold");
+const current0El = document.getElementById("current-0");
+const current1El = document.getElementById("current-1");
+const player0El = document.querySelector(".player-0");
+const player1El = document.querySelector(".player-1");
 
 //Starting conditions
 score0El.textContent = 0;
@@ -22,11 +22,11 @@ let activePlayer = 0;
 let playing = true;
 
 const switchPlayer = () => {
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  document.getElementById(`current-${activePlayer}`).textContent = 0;
   currentScore = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
-  player0El.classList.toggle("player--active");
-  player1El.classList.toggle("player--active");
+  player0El.classList.toggle("player-active");
+  player1El.classList.toggle("player-active");
 };
 
 // Rolling dice functionality
@@ -41,7 +41,7 @@ btnRoll.addEventListener("click", function () {
     if (dice !== 1) {
       // add dice value to current score
       currentScore += dice;
-      document.getElementById(`current--${activePlayer}`).textContent =
+      document.getElementById(`current-${activePlayer}`).textContent =
         currentScore;
     } else {
       // switch to next player
@@ -54,18 +54,18 @@ btnHold.addEventListener("click", function () {
   if (playing) {
     // 1. Add current score to active player
     scores[activePlayer] += currentScore;
-    document.getElementById(`score--${activePlayer}`).textContent =
+    document.getElementById(`score-${activePlayer}`).textContent =
       scores[activePlayer];
     // 2. check score is greater than 100
     if (scores[activePlayer] >= 100) {
       //    Finish game if greater than 100
       playing = false;
       document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.add("player--winner");
+        .querySelector(`.player-${activePlayer}`)
+        .classList.add("player-winner");
       document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.remove("player--active");
+        .querySelector(`.player-${activePlayer}`)
+        .classList.remove("player-active");
     } else {
       //    if not switch to next player
       switchPlayer();
